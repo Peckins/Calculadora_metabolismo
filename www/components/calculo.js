2406ab94c2e8dc320e2d284943d616ca41ebe37e
Peckins <messias.morais@etec.sp.gov.br>
2019-03-22 04:41:11 +0900

    Correção do javascript da página e adição de menu de seleção de nível de atividade

--- www/components/calculo.js
+++ www/components/calculo.js
@@ -1,23 +1,56 @@
 // This is a JavaScript file
 
-$(document).on("click",".btn",function(){
+//Verificando o sexo
 
-var peso = $("#peso").val();
-var altura = $("#altura").val();
-var idade = $("#idade").val();
-var sexo = $("#sexo").val();
+document.getElementsByName("input").value = null;//Limpa todos os input pela primeira vez
 
-  if(sexo != ""){
-      if(sexo == "masculino"){
-       $("#resultado").val(66+(13.7*peso)+(5.0 * altura) - (6.8 * idade));
-  }
+$(document).on("click", "#calcular", function(){
+
+  var taxaAtividade = $("#atividade").val();
+
+  var sexo = $("#sexo").val();
+
+    if (sexo != ""){
+
+      $("#sexo").css("border-color", "transparent");
+
+      if (taxaAtividade != ""){
+        $("#atividade").css("border-color", "transparent");
+        CalcularImb(sexo, taxaAtividade);
+      }
+      else{
+        alert("Selecione o nível de atividade!");
+        $("#atividade").css("border-color", "red");
+      }
+
+    }
     else{
-       $("#resultado").val(665 + (9.6 * peso) + (1.8 * altura) - (4.7 * idade));
-  }
+      alert("Informe seu sexo");
+      $("#sexo").css("border-color", "red");
+    }
 
-}
+});
+
+//Calculando o índice de acordo com o sexo
+function CalcularImb(sexo, taxaAtividade){
+
+  var peso = $("#peso").val();
+  var altura = $("#altura").val();
+  var idade = $("#idade").val();
+  var result;
+
+  if (sexo == "masculino"){
+    result = taxaAtividade * (66 + ((13.7 * peso) + (5 * altura) - (6.8 * idade)));
+  }
   else{
-    alert("Informe seu sexo");
+    result = taxaAtividade * (655 + ((9.6 * peso) + (1.8 * altura) - (4.7 * idade)));
   }
 
+  $("#resultado").val(result.toString());
+}
+
+//Limpando o formulário
+$(document).on("click", "#clear", function(){
+  $(".form-control").val(null);
+  $("select").css("border-color", "transparent");
 });
